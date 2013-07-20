b6ef818e9b9c474b1badb7362b4c9400bfeb50c6
sydcanem <icqhv.santos@gmail.com>
2013-07-20 12:01:07 +0800

    Initial release

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ split2obj
 =========
 
 A split stream [https://github.com/dominictarr/split] with default matcher that converts a line with whitespace separated words into a js object based on a supplied keys.
+
+Example:
+
+      github.com sydcanem split2obj
+
+      ...
+      .pipe(split2obj(['site', 'author', 'repository']))
+      .on('data', function (obj) {
+        console.dir(obj)
+        // result
+        // {site: "github.com", author: "sydcanem", repository: "split2obj"}
+      })
+
+Note:
+Not a node module yet so no `npm install` yet. Installation is to grab the repo itself and put the split2obj.js on your projects' lib directory.
---
+++ split2obj.js
@@ -0,0 +1,41 @@
+var split = require('split');
+
+/*
+	A split stream [https://github.com/dominictarr/split] with default matcher that converts
+	a line with whitespace separated words into a js object based on a supplied keys.
+*/
+
+function parseString (str) {
+    var re = /(?:")([^"]+)(?:")|([^\s"]+)(?=\s+|$)/g,
+    	res = [], 
+    	arr = null;
+
+    while (arr = re.exec(str)) { 
+    	res.push(arr[1] ? arr[1] : arr[0]); 
+    }
+
+    return res;
+}
+
+function split2obj (head, opts) {
+	if (!Array.isArray(head)) {
+		throw new Error('First argument must be an array.');
+	} 
+
+	var len = head.length;
+
+	function toObject (line) {
+		var obj = {},
+			td = parseString(line);
+
+		for (var i = 0; i < len; i += 1) {
+			obj[head[i]] = td[i];
+		}
+
+		return obj;
+	}
+
+	return split(toObject);
+}
+
+module.exports = cushion
